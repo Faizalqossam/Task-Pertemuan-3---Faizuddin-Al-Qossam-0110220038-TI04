146ca351571022678ea283c15e22f85e36faf111
Faizalqossam <72482067+Faizalqossam@users.noreply.github.com>
2021-10-18 10:57:07 +0700

    Add files via upload

---
+++ task.php
@@ -0,0 +1,60 @@
+<?php 
+#membuat class animal
+class Animal{
+    #property animals
+    public $animals = array();
+    #method construct - mengisi data awal
+    #parameter: data hewan (array)
+
+    public function __construct($animals){
+        $this->animals = ['Ayam', 'Ikan'];
+    }
+
+    #method index - menampilkan data animal
+    public function index(){
+        foreach ($this->animals as $this->index) {
+            echo "$this->index <br>";
+        }
+    }
+
+    #method store = menambahkan hewan baru
+    public function store($data){
+        array_push($this->animals, $data);
+    }
+
+    #method update - mengupdate hewan
+    #parameter:index dan hewan baru
+    public function update($index,$data){
+        unset($this->animals[$index]);
+        return $this->$index = array_push($this->animals, $data);
+    }
+
+    #method delete - menghapus hewan
+    #parameter: index
+    public function destroy($index){
+        return array_splice($this->animals, $index, 1);
+    }
+}
+
+# membuat object
+# kirimkan data hewan (array) ke constructor
+$animal = new Animal([]);
+
+echo "Index - Menampilkan seluruh hewan <br>";
+$animal->index();
+echo "<br>";
+
+echo "Store - Menambahkan hewan baru <br>";
+$animal->store('Burung');
+$animal->index();
+echo "<br>";
+
+echo "Update - Mengupdate hewan <br>";
+$animal->update(0,'Kucing Anggora');
+$animal->index();
+echo "<br>";
+
+echo "Destroy - Menghapus hewan <br>";
+$animal->destroy(0);
+$animal->index();
+echo "<br>";
